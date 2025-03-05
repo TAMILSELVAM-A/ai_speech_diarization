6978de649e8ea8d66209d01aab673d045e3164dd
TAMILSELVAM-A <tamilselvamcr72001@gmail.com>
2025-03-06 00:42:02 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,4 +20,4 @@ def read_root():
     return {"message": "Welcome to the Speaker Diarization API!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)

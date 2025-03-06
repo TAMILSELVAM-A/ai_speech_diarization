604b792cdfe6697ab3ef72c8abccf9cb0af1601d
TAMILSELVAM-A <tamilselvamcr72001@gmail.com>
2025-03-06 18:20:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from api.v1.endpoints import diarization
+import os
+
+app = FastAPI(title="Speaker Diarization API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(diarization.router, prefix="/api/v1", tags=["diarization"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Speaker Diarization API!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    # port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=8000, debug = True)

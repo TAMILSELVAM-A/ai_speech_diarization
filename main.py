2b5d4b6dfed3d2bfa0f4c1d4935c16d7691310db
TAMILSELVAM-A <tamilselvamcr72001@gmail.com>
2025-03-07 11:01:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,5 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
     # port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=8000, debug = True)
+    app.run(port=int(os.environ.get("PORT", 8000)),host='0.0.0.0',debug=True)
+
